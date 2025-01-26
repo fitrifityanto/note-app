fbfbba06cd189f800d98bd888724b35e68e9640b
fitrifityanto <fitrifityanto@gmail.com>
2025-01-26 20:16:58 +0700

    add/ custom component untuk form tambah note

--- src/script/components/index.js
+++ src/script/components/index.js
@@ -4,4 +4,5 @@ import "./footer-bar.js";
 import "./section-with-title.js";
 import "./note-loading.js";
 
+import "./note-form-tambah.js";
 import "./note-item.js";
---
+++ src/script/components/note-form-tambah.js
@@ -0,0 +1,59 @@
+class NoteFormTambah extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  _emptyContent() {
+    this.innerHTML = "";
+  }
+
+  connectedCallback() {
+    this.render();
+  }
+
+  render() {
+    this._emptyContent();
+
+    this.innerHTML = `
+        <form autocomplete="off">
+            <div class="form-group">
+              <label for="title">Judul</label>
+              <input
+                type="text"
+                id="title"
+                minlength="10"
+                aria-describedby="titleValidation"
+                required
+              />
+              <p
+                id="titleValidation"
+                class="validation-message"
+                aria-live="polite"
+              ></p>
+            </div>
+            <div class="form-group">
+              <label for="body">Isi</label>
+              <textarea
+                id="body"
+                name="body"
+                rows="4"
+                minlength="20"
+                maxlength="200"
+                aria-describedby="bodyValidation"
+                required
+              ></textarea>
+              <p
+                id="bodyValidation"
+                class="validation-message"
+                aria-live="polite"
+              ></p>
+            </div>
+            <div class="form-group">
+              <button id="buttonSave" class="btn">Simpan</button>
+            </div>
+          </form>
+        `;
+  }
+}
+
+customElements.define("note-form-tambah", NoteFormTambah);
